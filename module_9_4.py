ae1342dec97419a10ccb3d73139967fafb675836
Sheazer <57393800+Sheazer@users.noreply.github.com>
2024-10-17 22:07:28 +0600

    Add files via upload

---
+++ module_9_4.py
@@ -0,0 +1,33 @@
+first = 'Мама мыла раму'
+second = 'Рамена мало было'
+
+print(list(map(lambda x, y: x == y, first, second)))
+
+
+def get_advanced_writer(file_name):
+    def write_everything(*data_set):
+        with open(file_name, 'a', encoding='utf-8') as file:
+            for i in data_set:
+                file.write(str(i)+'\n')
+
+    return write_everything
+
+
+write = get_advanced_writer('example.txt')
+write('Это строчка', ['А', 'это', 'уже', 'число', 5, 'в', 'списке'])
+
+
+class MysticBall:
+    def __init__(self, *words):
+        self.words = tuple(words)
+
+    def __call__(self, *args, **kwargs):
+        return choice(self.words)
+
+
+from random import choice
+# Ваш класс здесь
+first_ball = MysticBall('Да', 'Нет', 'Наверное')
+print(first_ball())
+print(first_ball())
+print(first_ball())
